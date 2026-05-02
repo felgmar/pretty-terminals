49efd3d5ffe36b45cacb9d8d1385b012fd82076b
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2026-05-02 17:22:55 +0100

    Delete unused scripts

--- config/etc/profile.d/bash_aliases.sh
+++
@@ -1,7 +0,0 @@
-alias ls='ls --color=auto'
-alias ll='ls -alih --color=auto'
-
-alias ipkg='nala install'
-alias ppkg='nala purge --autoremove'
-alias updpkg='nala update'
-alias upgpkg='nala upgrade'
